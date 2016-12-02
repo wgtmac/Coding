a4422b26583e46030b333a3dbdd43c6bc4933001
wgtmac <ustcwg@gmail.com>
2016-12-01 22:06:44 -0800

    added 2 new questions

--- java/src/main/java/com/leetcode/IsSubsequence.java
+++ java/src/main/java/com/leetcode/IsSubsequence.java
@@ -1,33 +1,79 @@
-//package com.leetcode;
-//
-///**
-// * 392. Is Subsequence
-// *
-// * Given a string s and a string t, check if s is subsequence of t.
-// *
-// * You may assume that there is only lower case English letters in both s and t.
-// * t is potentially a very long (length ~= 500,000) string, and s is a short string (<=100).
-// *
-// * A subsequence of a string is a new string which is formed from the original
-// * string by deleting some (can be none) of the characters without disturbing the
-// * relative positions of the remaining characters. (ie, "ace" is a subsequence of
-// * "abcde" while "aec" is not).
-// *
-// * Example 1:
-// * s = "abc", t = "ahbgdc"
-// * Return true.
-// *
-// * Example 2:
-// * s = "axc", t = "ahbgdc"
-// * Return false.
-// *
-// * Follow up:
-// * If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you
-// * want to check one by one to see if T has its subsequence. In this scenario,
-// * how would you change your code?
-// */
-//public class IsSubsequence {
-//    public boolean isSubsequence(String s, String t) {
-//
-//    }
-//}
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 392. Is Subsequence
+ *
+ * Given a string s and a string t, check if s is subsequence of t.
+ *
+ * You may assume that there is only lower case English letters in both s and t.
+ * t is potentially a very long (length ~= 500,000) string, and s is a short string (<=100).
+ *
+ * A subsequence of a string is a new string which is formed from the original
+ * string by deleting some (can be none) of the characters without disturbing the
+ * relative positions of the remaining characters. (ie, "ace" is a subsequence of
+ * "abcde" while "aec" is not).
+ *
+ * Example 1:
+ * s = "abc", t = "ahbgdc"
+ * Return true.
+ *
+ * Example 2:
+ * s = "axc", t = "ahbgdc"
+ * Return false.
+ *
+ * Follow up:
+ * If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you
+ * want to check one by one to see if T has its subsequence. In this scenario,
+ * how would you change your code?
+ */
+public class IsSubsequence {
+    public boolean isSubsequence_GREEDY(String s, String t) {
+        int i = 0, j = 0;
+        while (i != s.length() && j != t.length()) {
+            if (s.charAt(i) == t.charAt(j++)) {
+                ++i;
+            }
+        }
+        return i == s.length();
+    }
+
+    public boolean isSubsequence(String s, String t) {
+        boolean[] f = new boolean[t.length() + 1], f_backup = new boolean[t.length() + 1];
+        Arrays.fill(f_backup, true);
+
+        for (int i = 1; i <= s.length(); ++i) {
+            for (int j = 1; j <= t.length(); ++j) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    f[j] = f_backup[j - 1];
+                } else {
+                    f[j] = f[j - 1];
+                }
+            }
+            boolean[] tmp = f_backup;
+            f_backup = f;
+            f = tmp;
+            Arrays.fill(f, false);
+        }
+
+        return f_backup[t.length()];
+    }
+
+    public boolean isSubsequence_MLE(String s, String t) {
+        boolean[][] f = new boolean[s.length() + 1][t.length() + 1];
+        Arrays.fill(f[0], true);
+
+        for (int i = 1; i <= s.length(); ++i) {
+            for (int j = 1; j <= t.length(); ++j) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    f[i][j] = f[i - 1][j - 1];    // same char, continue matching
+                } else {
+                    f[i][j] = f[i][j - 1];        // try to continue previous matching
+                }
+            }
+        }
+
+        return f[s.length()][t.length()];
+    }
+}
--- java/src/main/java/com/leetcode/PatchingArray.java
+++ java/src/main/java/com/leetcode/PatchingArray.java
@@ -1,60 +1,61 @@
-//package com.leetcode;
-//
-//import java.util.*;
-//
-///**
-// * 330. Patching Array
-// *
-// * Given a sorted positive integer array nums and an integer n, add/patch elements
-// * to the array such that any number in range [1, n] inclusive can be formed by
-// * the sum of some elements in the array. Return the minimum number of patches
-// * required.
-// *
-// * Example 1:
-// * nums = [1, 3], n = 6
-// * Return 1.
-// *
-// * Combinations of nums are [1], [3], [1,3], which form possible sums of: 1, 3, 4.
-// * Now if we add/patch 2 to nums, the combinations are: [1], [2], [3], [1,3], [2,3], [1,2,3].
-// * Possible sums are 1, 2, 3, 4, 5, 6, which now covers the range [1, 6].
-// * So we only need 1 patch.
-// *
-// * Example 2:
-// * nums = [1, 5, 10], n = 20
-// * Return 2.
-// * The two patches can be [2, 4].
-// *
-// * Example 3:
-// * nums = [1, 2, 2], n = 5
-// * Return 0.
-// */
-//public class PatchingArray {
-//    public int minPatches(int[] nums, int n) {
-//        // gather all numbers
-//        Set<Integer> sums = new HashSet<>();
-//        buildSum(nums, 0, 0, sums);
-//
-//        // find all missing numbers
-//        List<Integer> missing = new ArrayList<>();
-//        for (int i = n; i >= 1; --i) {
-//            if (!sums.contains(i)) missing.add(i);
-//        }
-//
-//
-//
-//
-//
-//
-//    }
-//
-//    // @param i: ith number in the array nums
-//    // @param sum: current sum
-//    private void buildSum(int[] nums, int i, int sum, Set<Integer> sums) {
-//        if (i == nums.length) {
-//            sums.add(sum);
-//        } else {
-//            buildSum(nums, i + 1, sum, sums);
-//            buildSum(nums, i + 1, sum + nums[i], sums);
-//        }
-//    }
-//}
+package com.leetcode;
+
+
+/**
+ * 330. Patching Array
+ *
+ * Given a sorted positive integer array nums and an integer n, add/patch elements
+ * to the array such that any number in range [1, n] inclusive can be formed by
+ * the sum of some elements in the array. Return the minimum number of patches
+ * required.
+ *
+ * Example 1:
+ * nums = [1, 3], n = 6
+ * Return 1.
+ *
+ * Combinations of nums are [1], [3], [1,3], which form possible sums of: 1, 3, 4.
+ * Now if we add/patch 2 to nums, the combinations are: [1], [2], [3], [1,3], [2,3], [1,2,3].
+ * Possible sums are 1, 2, 3, 4, 5, 6, which now covers the range [1, 6].
+ * So we only need 1 patch.
+ *
+ * Example 2:
+ * nums = [1, 5, 10], n = 20
+ * Return 2.
+ * The two patches can be [2, 4].
+ *
+ * Example 3:
+ * nums = [1, 2, 2], n = 5
+ * Return 0.
+ */
+
+/**
+ * A greedy solution
+ * @link: https://discuss.leetcode.com/topic/35494/solution-explanation
+ *
+ */
+public class PatchingArray {
+    public static void main(String[] args) {
+        PatchingArray p = new PatchingArray();
+        System.out.println(p.minPatches(new int[] {1, 3}, 6));
+        System.out.println(p.minPatches(new int[] {1, 5, 10}, 20));
+        System.out.println(p.minPatches(new int[] {1, 2, 2}, 5));
+    }
+
+    public int minPatches(int[] nums, int n) {
+        // initial range is [0, 1), after each iteration, the current guaranteed
+        // range is [0, miss). For next iteration, the updated range is
+        // [0, miss + nums[i]) when nums[i] < miss; otherwise miss should be patched
+        long miss = 1;
+        int patches = 0;
+        int i = 0;        // current index in nums, which is sorted
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss += nums[i++];
+            } else {
+                miss += miss;
+                patches++;
+            }
+        }
+        return patches;
+    }
+}
